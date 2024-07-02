f640ec69ebd7614ca41dfc271da64a98941d9526
PurpleSavage <jzuritapalomino@gmail.com>
2024-07-01 21:41:26 -0500

    cambio de los : por _ en la funcion de generar id aleatorio

--- src/generateUUID.py
+++ src/generateUUID.py
@@ -4,5 +4,6 @@ def generate_random_id():
     myuuid= uuid.uuid4()
     newuuid= str(myuuid).split("-")[0]
     currentdate = str(dt.datetime.now()).split(" ")
-    currenttime = currentdate[1].split(".")[0]
+    currenttime = currentdate[1].split(".")[0].replace(":","_")
     return f"{currentdate[0]}-{currenttime}-{newuuid}"
+
